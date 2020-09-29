fd816819348282c607a08caaeaf280927b439b2b
S A Sohan <50193747+shn999@users.noreply.github.com>
2020-09-29 11:58:39 +0600

    Update pms.py

--- userbot/modules/pms.py
+++ userbot/modules/pms.py
@@ -288,7 +288,7 @@ async def unblockpm(unblock):
             " was unblocc'd!.",
         )
 
-@register(incoming=True, outgoing=True, disable_edited=True)
+@register(incoming=True, disable_edited=True)
 async def monito_p_m_s(event):
     sender = await event.get_sender()
     if event.is_private and not (await event.get_sender()).bot:
